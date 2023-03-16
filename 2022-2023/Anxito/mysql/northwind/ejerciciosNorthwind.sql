ae199819c0c49e860b481204fbe4f0d66369ab15
Anxo Campos <anxo.campos260204@gmail.com>
2023-03-16 09:59:01 +0100

    all

--- 2022-2023/Anxito/mysql/northwind/ejerciciosNorthwind.sql
+++ 2022-2023/Anxito/mysql/northwind/ejerciciosNorthwind.sql
@@ -1,70 +1,80 @@
-# 1- Realizar una consulta que me muestre toda las columnas de la tabla de productos
-SELECT *
-FROM products;
+#1
+SELECT * FROM products;
 
-# 2- Realizar una consulta que me muestre el código, el nombre y el precio del 
-#producto
-SELECT ProductName, ProductID, UnitPrice
-FROM products;
+#2
+SELECT productID,productName,unitPrice from Products;
 
-#3- Realizar una consulta que me muestre el código, nombre y la categoría de los 
-#productos que estén entre 18.00 y 20.00
+#3
+SELECT productID,productName,categoryID,unitPrice FROM products WHERE unitPrice between 18 AND 20;
+#EJEMPLOS DE CÓMO SACAR EL NOMBRE DE LA CATEOGORÍA CON UN JOIN Y CON UNA SUBCONSULTA
+SELECT ProductID,ProductName,categoryName FROM products JOIN categories USING(CategoryID);
+SELECT ProductID,ProductName,(SELECT categoryName FROM categories WHERE categoryID=p.categoryID) FROM products p;
 
-SELECT ProductId, ProductName, CategoryName
-FROM products p JOIN Categories c USING(categoryID) 
-WHERE unitPrice between 18.000 and 20.000;
+#4 ACORDAOS DE LA IMPORTANCIA DEL DUAL
 
-# 4- Realiza una consulta que permita presentar la fecha actual mediante una función
-SELECT SYSDATE(), curdate(), current_date()
-FROM dual;
+SELECT SYSDATE(),curdate(),current_date() FROM dual;
 
-#5- Realiza una consulta que permita presentar los apellidos de los empleados en 
-#minúscula
-SELECT LOWER(LastName)
+#5 Consulta optimizada, ENTRA EN EXAMEN, REVISAR TEORÍA
+
+(SELECT CONCAT(LOWER(UPPER(LOWER(UPPER(lastName)))),UPPER(SUBSTRING(firstname,0,0))) FROM Employees LIMIT 0,5)
+UNION
+(SELECT CONCAT(LOWER(UPPER(LOWER(UPPER(lastName)))),UPPER(SUBSTRING(firstname,0,0))) FROM Employees LIMIT 5,9);
+
+#6 VERSIÓN PARA NOTA, CAERÁ UNA SIMILAR
+
+SELECT UPPER(optimizada.apellido)
+FROM (SELECT CONCAT(LOWER(UPPER(LOWER(UPPER(lastName)))),UPPER(SUBSTRING(firstname,0,0))) AS apellido FROM Employees) optimizada;
+
+#7
+
+SELECT substring(firstName,1,3)
 FROM employees;
 
+#8
 
-#6- Realiza una consulta que permita presentar los apellidos de los empleados en 
-#mayúscula
-SELECT UPPER(LastName)
-FROM employees;
-
-#7- Realiza una consulta que permita presentar las tres primeras letras del nombre de 
-#los empleados
-SELECT FirstName, SUBSTRING(FirstName, 1, 3)
-FROM employees;
-
-#8- Realiza una consulta que permita presentar el nombre y apellido, y un correo 
-#electrónico de los empleados, éste deberá tener los siguientes datos:
-#a. La primera letra del nombre, seguida del apellido
-#b. El correo deberá presentarse en minúscula
-
-SELECT   firstname, lastName,LOWER(CONCAT(SUBSTRING(firstname,1,1), lastName,"@northwind.com")) AS Email
-FROM employees ; 
-
-#9- Realiza una consulta que permita presentar el nombre y apellidos de los 
-#empleados que tengan más de 40 años
-SELECT CONCAT(firstName, ' ', lastName) AS 'nombre' ,YEAR(curdate()) - year(BirthDate)>=40
+SELECT firstName,LastName,LOWER(CONCAT(SUBSTRING(firstName,1,1),lastName,"@northwind.com")) AS Email
 FROM Employees;
 
+#9
+
+SELECT FirstName,LastName,BirthDate,YEAR(curdate())-YEAR(birthdate) AS EDAD_NO_EXACTA,ROUND((DATEDIFF(curdate(),birthdate)/365),0),timestampdiff(YEAR ,birthdate,curdate())  FROM employees;
+
 SELECT YEAR(orderDate),MONTH(orderDate) AS Mes,COUNT(*)
-FROM orders
-GROUP BY YEAR(orderDate),MONTH(orderDate) HAVING MES=3;
+FROM orders GROUP BY YEAR(orderDate),MONTH(orderDate) HAVING MES=3;
 
 SELECT YEAR(orderDate),COUNT(*)
 FROM orders
-WHERE MONTH (orderDate)=3
+WHERE MONTH(orderDate)=3
 GROUP BY YEAR(orderDate);
 
-#10- Realiza una consulta que permita presentar el número de fax que tiene cada país
+#10
 
+SELECT country,COUNT(DISTINCT FAX)
+FROM customers
+GROUP BY country;
 
-#11- Realiza una consulta que permita presentar el máximo, mínimo y el promedio de 
-#años que llevan en la empresa los empleados
+#11
+#Forma eficiente con subconsulta
+SELECT MAX(anhos.antiguedad),MIN(anhos.antiguedad),AVG(antiguedad)
+FROM (SELECT hireDate,timestampdiff(YEAR,hiredate,curdate()) AS Antiguedad
+ FROM employees) anhos;
+ 
+#Forma menos eficiente calculando en la select todos los datos
 
+SELECT MIN(timestampdiff(YEAR,hiredate,curdate())) AS Rookie,MAX(timestampdiff(YEAR,hiredate,curdate())) AS Senior, AVG(timestampdiff(YEAR,hiredate,curdate())) AS "MEDIA EMPRESA"
+FROM employees;
 
-#12- Realiza una consulta que permita presentar un listado de países que no tienen 
-#faxes con su respectiva cantidad
+#12
+
+SELECT country,COUNT(*)
+FROM customers c1
+GROUP BY country
+HAVING COUNT(*)=(SELECT COUNT(*) FROM customers c2  WHERE c2.fax="" AND C2.country=c1.country GROUP BY c2.country);
+
+SELECT country,group_concat(DISTINCT fax) AS faxes
+FROM customers
+GROUP BY country
+HAVING faxes="";
 
 
 #13- Realiza una consulta que permita presentar el nombre del cliente, el fax y el país 
