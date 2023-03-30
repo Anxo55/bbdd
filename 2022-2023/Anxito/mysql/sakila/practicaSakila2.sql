ac809b3ec0e21723be223701759fc043198a68ce
Anxo Campos <anxo.campos260204@gmail.com>
2023-03-30 08:35:53 +0200

    all

--- 2022-2023/Anxito/mysql/sakila/practicaSakila2.sql
+++ 2022-2023/Anxito/mysql/sakila/practicaSakila2.sql
@@ -1,5 +1,6 @@
 #1 Saca las 10 primeras películas alfabéticamente y el número de copias que se disponen de cada  una de ellas.
-SELECT fa.actor_id AS ID,CONCAT(a.first_name," ",a.last_Name) AS Nombre,COUNT(*) AS "Número de películas" FROM film_actor fa JOIN actor a USING(actor_id)
+SELECT fa.actor_id AS ID,CONCAT(a.first_name," ",a.last_Name) AS Nombre,COUNT(*) AS "Número de películas" 
+FROM film_actor fa JOIN actor a USING(actor_id)
 GROUP BY fa.actor_id
 ORDER BY 3 DESC LIMIT 10;
 
